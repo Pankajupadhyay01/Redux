24ca7a00d3a73cfce25fcc222f762448ddff514f
pankajupadhyay01 <pankajup.git01@gmail.com>
2023-07-16 22:36:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() { 
+
+  return (
+    <>
+     <p className=" font-extrabold text-5xl ">text</p>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = { 
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    fontFamily: {
+      inter: ["inter","serif","inherit"],
+    },
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [], 
+ 
+}
